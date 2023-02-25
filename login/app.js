8bc4fbb5193a5891d7da643b9e04708ef1ce40c0
jackmmin <jongmin9449@gmail.com>
2023-02-25 23:17:35 +0900

    http로 서버 구동시키기

--- login/app.js
+++ login/app.js
@@ -1,6 +1,15 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const app = http.createServer((req, res) => {
+    // 한글 처리
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    
+    if(req.url === "/"){
+        res.end("여기는 루트입니다.");
+    }else if(req.url === "/login"){
+        res.end("여기는 로그인 화면입니다.");
+    }
+});
 
-app.listen(3000, function() {
-    console.log("서버 가동");
+app.listen(3001, ( )=> {
+    console.log("https로 가동된 서버입니다.");
 });
