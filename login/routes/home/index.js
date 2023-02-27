8e826b76ea54913c9d73b90f9d81187b41c6c4a7
jackmmin <jongmin9449@gmail.com>
2023-02-27 16:11:02 +0900

    컨트롤러 분리

--- login/routes/home/index.js
+++ login/routes/home/index.js
@@ -3,11 +3,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("home/index");
-});
-router.get("/login", (req, res) => {
-    res.render("home/login");
-});
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.hello);
+router.get("/login", ctrl.login);
 
 module.exports = router; // 외부에서 사용하기 위해 내보내기
