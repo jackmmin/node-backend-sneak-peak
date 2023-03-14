1dce10c1b6d9499d4562898dfda0fe0d5ecb69ed
jackmmin <jongmin9449@gmail.com>
2023-03-14 21:38:31 +0900

    데이터 파일로 관리

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,15 @@ class User {
 
     login(){
         const client = this.body;
-        const {id, password} = UserStorage.getUserInfo(client.id);
+        console.log(UserStorage.getUserInfo(client.id));
         
-        if(id){ // id가 존재하면
-            if(id === client.id && password === client.password){ // id가 일치하고 password가 일치하면
-                return {success: true};
-            } // password가 일치하지 않으면
-            return {success: false, msg: "비밀번호가 틀렸습니다."};
-        } // id가 존재하지 않으면
-        return {success: false, msg: "존재하지 않는 아이디입니다."};
+        // if(id){ // id가 존재하면
+        //     if(id === client.id && password === client.password){ // id가 일치하고 password가 일치하면
+        //         return {success: true};
+        //     } // password가 일치하지 않으면
+        //     return {success: false, msg: "비밀번호가 틀렸습니다."};
+        // } // id가 존재하지 않으면
+        // return {success: false, msg: "존재하지 않는 아이디입니다."};
     }
     
     register(){
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,18 +1,15 @@
 "use strict";
 
+const fs = require("fs");
+
 class UserStorage{
     // static은 따로 new로 생성하지 않아도 접근할 수 있도록 해준다.
     // 하지만 그렇게 되면 바로 데이터에 접근할 수 있기 때문에 #(은닉화)을 사용하여 undefined로 출력되게 하고
     // 따로 출력함수를 만들어 은닉화된 데이터를 사용할 수 있게 해준다.
-    static #users = { // #은 은닉화를 의미.
-        id: ["jongmin", "sungjin", "bumyoung"],
-        password: ["1234", "1234", "123456"],
-        name: ["종민", "성진", "범영"]
-    }
-
+    
     // 은닉화된 데이터에 접근하기 위한 함수
     static getUsers(...fields){
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -23,19 +20,24 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, key) => {
-            newUser[key] = users[key][idx];
-            return newUser;
-        }, {});
+        return fs.readFile("./src/databases/users.json", (err, data) => {
+            if (err) throw error;
+            const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const usersKeys = Object.keys(users); // [id, password, name]
+            const userInfo = usersKeys.reduce((newUser, key) => {
+                newUser[key] = users[key][idx];
+                return newUser;
+            }, {});
 
-        return userInfo;
+            return userInfo;
+        });
+
+        
     }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
