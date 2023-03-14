f785922e3fc893c5adaa1f53e255abab4e8c6592
jackmmin <jongmin9449@gmail.com>
2023-03-14 21:18:32 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,16 +8,22 @@ class User {
     }
 
     login(){
-        const body = this.body;
-        const {id, password} = UserStorage.getUserInfo(body.id);
+        const client = this.body;
+        const {id, password} = UserStorage.getUserInfo(client.id);
         
         if(id){ // id가 존재하면
-            if(id === body.id && password === body.password){ // id가 일치하고 password가 일치하면
+            if(id === client.id && password === client.password){ // id가 일치하고 password가 일치하면
                 return {success: true};
             } // password가 일치하지 않으면
             return {success: false, msg: "비밀번호가 틀렸습니다."};
         } // id가 존재하지 않으면
         return {success: false, msg: "존재하지 않는 아이디입니다."};
+    }
+    
+    register(){
+        const client = this.body;
+        const response = UserStorage.save(client);
+        return response;
     }
 }
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,7 +7,9 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.show.hello);
 router.get("/login", ctrl.show.login);
-router.post("/register", ctrl.show.register);
+router.get("/register", ctrl.show.register);
+
 router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
 
 module.exports = router; // 외부에서 사용하기 위해 내보내기
