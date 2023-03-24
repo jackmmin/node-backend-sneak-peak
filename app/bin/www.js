9eb90abad95fb67a7ba2193e6459942134b5a224
jackmmin <jongmin9449@gmail.com>
2023-03-24 16:23:00 +0900

    AWS RDS 새로 생성 후 커밋

--- app/bin/www.js
+++ app/bin/www.js
@@ -2,6 +2,7 @@
 
 const app = require("../app");
 // process.env 을 사용하면 현재 어플리케이션의 .env 파일로 접근이 가능하다.
+// process.env 에서 PORT를 찾으면 그 값을, 없으면 3000을 대입
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
