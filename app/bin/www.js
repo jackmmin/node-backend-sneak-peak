c25767a1e2535f9a0a2cf4c9702ef5e772eeb4b9
jackmmin <jongmin9449@gmail.com>
2023-03-24 15:46:53 +0900

    민감정보 삭제

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const app = require("../app");
-const PORT = 3000;
+// process.env 을 사용하면 현재 어플리케이션의 .env 파일로 접근이 가능하다.
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("서버 가동");
