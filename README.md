a2d1186d7dfa8cf2fdff45c1b602cf00aadad933
jackmmin <jongmin9449@gmail.com>
2023-03-28 15:42:06 +0900

    로그관리 winston 2/2 등록 및 설정

--- README.md
+++ README.md
@@ -20,7 +20,8 @@
 17. User 모델 구현<br>
 18. 로그인 화면 꾸미기<br>
 19. 회원가입 페이지 구현<br>
-20. AWS RDS 데이터베이스 <br>
+20. AWS RDS 데이터베이스 구축 및 연동<br>
+21. 로그관리<br>
 위 항목들을 학습합니다.
 
 > 출처: https://youtube.com/playlist?list=PLSK4WsJ8JS4cQ-niGNum4bkK_THHOizTs
--- app/src/config/logger.js
+++ app/src/config/logger.js
@@ -1,28 +1,49 @@
 const {createLogger, transports, format} = require("winston");
-const {combine, timestamp, label, printf, json, simple, colorize} = format;
+const {combine, timestamp, label, printf, simple, colorize} = format;
 
 const printFormat = printf(({timestamp, label, level, message}) => {
     return `${timestamp} [${label}] ${level} : ${message}`;
 });
 
-const printLogFormat = combine(
-    label({
-        label: "백엔드 맛보기",
+const printLogFormat = {
+    file: combine(
+        label({
+            label: "백엔드 맛보기",
+        }),
+        timestamp({
+            format: "YYYY-MM-DD HH:mm:dd",
+        }),
+        printFormat,
+    ),
+
+    console: combine(
+        colorize(),
+        simple()
+    ),
+};
+
+const opts = {
+    file: new transports.File({
+        filename: "access.log",
+        dirname: "./log",
+        options: {encoding: 'utf8'},
+        level: "info",
+        format: printLogFormat.file,
     }),
-    colorize(),
-    timestamp({
-        format: "YYYY-MM-DD HH:mm:dd",
+
+    console: new transports.Console({
+        options: {encoding: 'utf8'},
+        level: "info",
+        format: printLogFormat.console,
     }),
-    printFormat,
-);
+}
 
 const logger = createLogger({
-    transports: [
-        new transports.Console({
-            level: "info",
-            format: printLogFormat,
-        }),
-    ],
+    transports: [opts.file],
 });
+
+if(process.env.NODE_ENV !== "production"){
+    logger.add(opts.console);
+};
 
 module.exports = logger;
