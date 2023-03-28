1089c93e82db381b9535c5d5123ce1ffd4448383
jackmmin <jongmin9449@gmail.com>
2023-03-28 15:22:18 +0900

    로그관리 winston 1/2

--- app/app.js
+++ app/app.js
@@ -15,7 +15,8 @@ const accessLogStream = require("./src/config/log");
 // 라우팅
 const home = require("./src/routes/home");
 
-
+const logger = require("./src/config/logger");
+logger.info("Hello World");
 
 // 웹 세팅
 app.set("views", "./src/views");
